a0b6d8716a713a2b34302e82063b24e4f35bf9f1
Michał Mosiołek <17131308+bittz@users.noreply.github.com>
2024-08-26 12:17:19 +0200

    Print CSV file

---
+++ csv_01.c
@@ -0,0 +1,42 @@
+// Print CSV file
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+int main()
+{
+    const char *filename = "./games.csv";
+    errno = 0;
+
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
+        fprintf(stderr, "Unable to open the file %s. %s\n", filename, strerror(errno));
+        exit(1);
+    }
+
+    int c, in_word = 0;
+
+    while ((c = getc(fp)) != EOF)
+    {
+        if (c == '"')
+            in_word = !in_word;
+        else if (!in_word && c == ',')
+            putchar('\t');
+        else if (in_word || c == '\n')
+            putchar(c);
+    }
+
+    if (ferror(fp))
+    {
+        puts("I/O while reading");
+        exit(1);
+    }
+    else if (feof(fp))
+        puts("End of file reached");
+
+    fclose(fp);
+
+    return 0;
+}
